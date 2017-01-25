ac8d59e944679e17532d5c30046fd65509bf918d
Artem Zatcepa <gefreiterdinets@gmail.com>
2017-01-25 22:19:28 +0200

    edit posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,15 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -17,6 +22,23 @@ class PostsController < ApplicationController
     else
       render "new"
     end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
