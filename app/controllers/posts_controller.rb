54688e389a4ec61f1d11e3ad25bded9deaae3f13
Donets <gefreiterdinets@gmail.com>
2017-02-06 16:26:05 +0200

    fix deleted user id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(@post.user_id)
+    # @user = User.find(@post.user_id)
     @comments = Comment.where(post_id: @post.id)
   end
 
