ab195519ae39da1b573893f5d184afc34f3fd707
AZatsepa <artem.zatsepa@nixsolutions.com>
2018-01-07 02:20:31 +0200

    fix dotenv requirements

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+Dotenv::Railtie.load if Rails.env.test? || Rails.env.development?
 
 module Museum
   class Application < Rails::Application
