84f2d4c6a6769ab5f30137ba52a55538fc60f36f
zatsepa <artem.zatsepa@nixsolution.com>
2018-05-10 18:51:05 +0300

    add profile changing

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -25,4 +25,40 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+
+  before_action :set_user
+
+  def facebook
+    return unless @user.persisted?
+    sign_in_and_redirect @user, event: :authentication
+    set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+  end
+
+  def google_oauth2
+    return unless @user.persisted?
+    sign_in_and_redirect @user, event: :authentication
+    set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+  end
+
+  private
+
+  def set_user
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+  end
+
+  def after_sign_in_path_for(resource)
+    if resource.authentications.count != 0
+      update_resource(resource)
+      super
+    elsif resource.first_name.blank? || resource.last_name.blank?
+      edit_user_registration_path(resource)
+    else
+      super
+    end
+  end
+
+  def update_resource(resource)
+    resource.update_attributes!(first_name: request.env['omniauth.auth'].info.first_name,
+                                last_name: request.env['omniauth.auth'].info.last_name)
+  end
 end
--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -3,15 +3,23 @@ feature 'User registration', %q(
   I want to create a new account
 ) do
   context 'when valid steps' do
-    scenario 'should create unconfirmed user' do
+    before do
       visit new_user_registration_path
 
       fill_in t('registration.email'), with: 'test@test.com'
       fill_in t('registration.password'), with: '12345678'
       fill_in t('registration.password_confirmation'), with: '12345678'
       click_on t('registration.sign_up')
-
+    end
+    scenario 'should create unconfirmed user' do
       expect(User.find_by(email: 'test@test.com')).to_not be_confirmed
+    end
+
+    scenario 'should redirect to edit profile page' do
+      user = User.find_by(email: 'test@test.com')
+      token = user.confirmation_token
+      visit user_confirmation_url(confirmation_token: token)
+      expect(current_path).to eql edit_user_registration_path(user)
     end
   end
 
