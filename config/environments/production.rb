a0d63d526c47f30bb0e12d9f165be1e0634e00ef
zatsepa <artem.zatsepa@nixsolution.com>
2018-05-10 14:06:45 +0300

    add smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,19 @@ Rails.application.configure do
   # (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "museum_#{Rails.env}"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: ENV['DEFAULT_URL_HOST'] }
+  config.action_mailer.smtp_settings = {
+    port: ENV['SMTP_PORT'].to_i,
+    address: ENV['SMTP_HOST'],
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    domain: ENV['SMTP_DOMAIN'],
+    enable_starttls_auto: true,
+    authentication: 'plain'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server
