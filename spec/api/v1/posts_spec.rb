34d1128f3e3fe35e3c39b52589251dcab8c1e924
zatsepa <artem.zatsepa@nixsolution.com>
2018-05-29 18:21:10 +0300

    add /api/v1/posts/:id endpoint

--- spec/api/v1/posts_spec.rb
+++ spec/api/v1/posts_spec.rb
@@ -53,4 +53,51 @@ describe 'Posts API' do
       end
     end
   end
+
+  describe 'GET /posts/:id' do
+    let(:user) { create(:user) }
+    let(:access_token) { create(:access_token) }
+    let!(:posts) { create_list(:post, POSTS_COUNT, user: user) }
+    let(:post) { posts.first }
+    let!(:comment) { create(:comment, user: user, post: post) }
+
+    context 'unauthorized' do
+      it 'should return 401 status if there is no access token' do
+        get "/api/v1/posts/#{post.id}", params: { format: :json }
+        expect(response.status).to eql 401
+      end
+
+      it 'should return 401 status if access token is invalid' do
+        get "/api/v1/posts/#{post.id}", params: { format: :json, access_token: '1234' }
+        expect(response.status).to eql 401
+      end
+    end
+
+    context 'authorized' do
+      before { get "/api/v1/posts/#{post.id}", params: { format: :json, access_token: access_token.token } }
+
+      it 'should return 200 status' do
+        expect(response).to be_successful
+      end
+
+      POST_ALLOWED_FIELDS.each do |field|
+        it "should contain #{field}" do
+          expect(response.body).to be_json_eql(post.send(field.to_sym).to_json).at_path("post/#{field}")
+        end
+      end
+
+      context 'comments' do
+        it 'should be included in post' do
+          expect(response.body).to have_json_size(1).at_path('post/comments')
+        end
+
+        COMMENT_ALLOWED_FIELDS.each do |field|
+          it "should contain #{field}" do
+            expect(response.body).to(be_json_eql(comment.send(field.to_sym).to_json)
+                                       .at_path("post/comments/0/#{field}"))
+          end
+        end
+      end
+    end
+  end
 end
