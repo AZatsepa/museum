f6dabb9aeddc7ff464c84cd508f0e8149b96f49c
zatsepa <artem.zatsepa@nixsolution.com>
2018-05-29 15:15:21 +0300

    add GET /api/v1/other_users endpoint

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,5 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :email, :first_name, :last_name, :nickname, :image_url, :role, :created_at, :updated_at, :confirmed_at
+  has_many :posts
+  has_many :comments
+end
--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resource :profiles do
         get 'me', to: 'profiles#me'
+        get 'other_users', to: 'profiles#other_users'
       end
     end
   end
