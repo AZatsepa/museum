38b01884d67ad87a83770a551f6b25720c5c1dc2
zatsepa <artem.zatsepa@nixsolutions.com>
2018-12-28 13:59:30 +0200

    Add validation to create_user form

--- app/javascript/packs/validations.js
+++ app/javascript/packs/validations.js
@@ -21,4 +21,37 @@ $(window).bind('turbolinks:load', () => {
       },
     },
   });
+
+  $('#create_user').validate({
+    rules: {
+      'user[email]': {
+        required: true,
+        email: true,
+      },
+      'user[password]': {
+        required: true,
+        minlength: 6,
+      },
+      'user[password_confirmation]': {
+        required: true,
+        minlength: 6,
+        equalTo: 'user[password]',
+      },
+    },
+    messages: {
+      'user[email]': {
+        required: I18n.t('errors.form.email_format'),
+        email: I18n.t('errors.blank'),
+      },
+      'user[password]': {
+        required: I18n.t('errors.blank'),
+        minlength: I18n.t('errors.min_length', { number: 6 }),
+      },
+      'user[password_confirmation]': {
+        required: I18n.t('errors.blank'),
+        minlength: I18n.t('errors.min_length', { number: 6 }),
+        equalTo: I18n.t('errors.form.password_confirmation'),
+      },
+    },
+  });
 });
