f45ee8a574ed582b9014157f21bd75eb2d91223a
zatsepa <artem.zatsepa@nixsolutions.com>
2018-12-28 16:15:37 +0200

    Add validation to edit profile

--- app/javascript/packs/validations.js
+++ app/javascript/packs/validations.js
@@ -84,4 +84,60 @@ $(window).bind('turbolinks:load', () => {
       },
     },
   });
+  $('#edit_user').validate({
+    rules: {
+      'user[email]': {
+        required: true,
+        email: true,
+      },
+      'user[nickname]': {
+        required() {
+          return $('#user_first_name').val().length === 0 || $('#user_last_name').val().length === 0;
+        },
+      },
+      'user[first_name]': {
+        required() {
+          return $('#user_last_name').val().length === 0 && $('#user_nickname').val().length === 0;
+        },
+      },
+      'user[last_name]': {
+        required() {
+          return $('#user_first_name').val().length === 0 && $('#user_nickname').val().length === 0;
+        },
+      },
+      'user[password]': {
+        minlength: 6,
+      },
+      'user[password_confirmation]': {
+        minlength: 6,
+        equalTo: {
+          param: '#user_password',
+          depends() {
+            return $('#user_password').val().length > 0;
+          },
+        },
+      },
+      'user[current_password]': {
+        required: true,
+      },
+    },
+    messages: {
+      'user[email]': {
+        required: I18n.t('errors.form.email_format'),
+        email: I18n.t('errors.blank'),
+      },
+      'user[nickname]': I18n.t('errors.form.nickname'),
+      'user[first_name]': I18n.t('errors.form.first_name'),
+      'user[last_name]': I18n.t('errors.form.last_name'),
+      'user[current_password]': I18n.t('errors.form.current_password'),
+      'user[password]': {
+        minlength: I18n.t('errors.min_length', { number: 6 }),
+      },
+      'user[password_confirmation]': {
+        required: I18n.t('errors.blank'),
+        minlength: I18n.t('errors.min_length', { number: 6 }),
+        equalTo: I18n.t('errors.form.password_confirmation'),
+      },
+    },
+  });
 });
