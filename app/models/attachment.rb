7774ec2818cd687c4fe07b3fcdcacc783bf1238b
zatsepa <artem.zatsepa@nixsolutions.com>
2019-01-10 18:11:49 +0200

    Add master.key

--- app/models/attachment.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Attachment < ApplicationRecord
-  delegate :identifier, to: :file
-  belongs_to :attachable, polymorphic: true, optional: true
-  mount_uploader :file, FileUploader
-  validates :file, presence: true
-end
