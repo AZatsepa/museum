5ebade6bd618ad6c2d52ceb303c62921d31b3f31
AZatsepa <artem.zatsepa@gmail.com>
2019-12-02 23:18:02 +0200

    Configure Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 if Rails.env.production?
-  if Rails.application.credentials[:sentry_dsn].nil?
+  if Rails.application.credentials.dig(Rails.env.to_sym, :sentry_dsn).nil?
     Rails.logger.info 'No SENTRY_DSN available, skipping configuring sentry'
   else
     Raven.configure do |config|
-      config.dsn = Rails.application.credentials[Rails.env.to_sym][:sentry_dsn]
+      config.dsn = Rails.application.credentials.dig(Rails.env.to_sym, :sentry_dsn)
     end
   end
 end
