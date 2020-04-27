e12a944a6afc6b355867c71c49772013c95a6dbe
AZatsepa <artem.zatsepa@gmail.com>
2020-04-28 00:27:45 +0300

    Add old plans

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,24 @@ Rails.application.routes.draw do
     get 'comparison/1943_2017', to: 'comparison#_1943_2017'
     get 'comparison',           to: 'comparison#index'
     get 'comparison/1782_2017', to: 'comparison#_1782_2017'
-    get 'maps/1782',            to: 'maps#map_1782'
+
+    resources :maps, only: %i[index] do
+      get 'map_1740',            to: 'maps#map_1740',            on: :collection
+      get 'map_1752',            to: 'maps#map_1752',            on: :collection
+      get 'map_1761',            to: 'maps#map_1761',            on: :collection
+      get 'map_1766',            to: 'maps#map_1766',            on: :collection
+      get 'map_1768',            to: 'maps#map_1768',            on: :collection
+      get 'map_1770',            to: 'maps#map_1770',            on: :collection
+      get 'map_1782',            to: 'maps#map_1782',            on: :collection
+      get 'map_1786',            to: 'maps#map_1786',            on: :collection
+      get 'map_1787_1786',       to: 'maps#map_1787_1786',       on: :collection
+      get 'map_1845',            to: 'maps#map_1845',            on: :collection
+      get 'map_1910',            to: 'maps#map_1910',            on: :collection
+      get 'map_moscovian_crimean_gates', to: 'maps#map_moscovian_crimean_gates', on: :collection
+      get 'map_unknown_year',    to: 'maps#map_unknown_year',    on: :collection
+      get 'map_unknown_year_2',  to: 'maps#map_unknown_year_2',  on: :collection
+      get 'map_unknown_year_3',  to: 'maps#map_unknown_year_3',  on: :collection
+    end
 
     namespace :api do
       namespace :v1 do
