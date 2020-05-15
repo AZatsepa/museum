60540e8093eacddf5e24342a57a62b04921faf6d
AZatsepa <artem.zatsepa@gmail.com>
2020-05-15 21:03:48 +0300

    Set up Cap task

--- config/deploy.rb
+++ config/deploy.rb
@@ -83,13 +83,15 @@ namespace :deploy do
   after  :finishing,    :cleanup
 end
 
-after 'deploy:update_code', 'sitemaps:create_symlink'
+after 'deploy:updating', 'sitemaps:create_symlink'
 
 namespace :sitemaps do
-  task :create_symlink, roles: :app do
-    run "mkdir -p #{shared_path}/sitemaps"
-    run "rm -rf #{release_path}/public/sitemaps"
-    run "ln -s #{shared_path}/sitemaps #{release_path}/public/sitemaps"
+  task :create_symlink do
+    on roles(:app) do
+      execute "mkdir -p #{shared_path}/sitemaps"
+      execute "rm -rf #{release_path}/public/sitemaps"
+      execute "ln -s #{shared_path}/sitemaps #{release_path}/public/sitemaps"
+    end
   end
 end
 
